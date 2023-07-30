7c5fb00c18c1efedf44d5574833d2da66faa8949
aki <agosight0307@gmail.com>
2023-07-30 22:12:47 +0900

    get quiz data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import fetch from 'node-fetch';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async requestData(): Promise<object> {
+    try {
+      const response = await fetch(
+        'https://opentdb.com/api.php?amount=10&type=multiple',
+      );
+      const text = await response.text();
+      const fetchText = JSON.parse(text);
+      return fetchText;
+    } catch (error) {
+      throw new Error('Failed to fetch data');
+    }
   }
 }
